d09a67ce9a13ba49fecffb63f6ffcf276071cc27
homuhomy <74242263+homuhomy@users.noreply.github.com>
2022-01-01 00:07:40 +0800

    update

---
+++ src/sameBehaviour.java
@@ -0,0 +1,21 @@
+public abstract class sameBehaviour {
+    protected int atkPoint;
+    protected float critChance = 0.1f;
+    protected float accuracy = 0.8f;
+
+    /**
+     * Displays the stats
+     *
+     */
+    public abstract void displayStats();
+
+
+    //getters
+    public float getCritChance() {
+        return critChance;
+    }
+
+    public int getAtkPoint() {
+        return atkPoint;
+    }
+}
