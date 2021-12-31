2e8e782447c1e78b158c798050d153c297df35d5
homuhomy <74242263+homuhomy@users.noreply.github.com>
2021-12-31 19:35:51 +0800

    update

--- src/Game.java
+++ src/Game.java
@@ -4,17 +4,24 @@ import java.awt.*;
 public class Game {
 
     JFrame window;
+    Container con1;
 
     public static void main(String[] args) {
         new Game(); //call title
     }
 
+    //to create a window
     public Game(){
+
         //create new window
-        window = new JFrame;
-        window.setSize(1000,800);
+        window = new JFrame();
+        window.setSize(1000,800); //discuss what size for the window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setBackground(new Color());
+        window.getContentPane().setBackground(new Color(109, 215, 254, 179)); //using RGB
+        window.setLayout(null); //to use custom layout
+        window.setVisible(true);
+
+        con1 = window.getContentPane();
 
     }
 }
