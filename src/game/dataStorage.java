5a099c9fc0edc6dea990c7710aad8c2c5bbaf5a4
Penjinak Meow <izzulizzani2002@gmail.com>
2022-01-15 20:44:48 +0800

    test 2

---
+++ src/game/dataStorage.java
@@ -0,0 +1,20 @@
+package game;
+
+import java.io.Serializable;
+
+public class dataStorage implements Serializable {
+    //menu stats
+    int tax,year,currentSeason,gold;
+    String currentEvent;
+    //dragon stats
+    int dragonAttackPoint,dragonLevel,dragonHP;
+    float dragonCritChance,dragonAccuracy;
+    //wall stats
+    int wallHP;
+    float wallBlockPercentage;
+    //tower stats
+    int towerAttackPoint;
+    float towerCritChance,towerAccuracy;
+    //emotions stats
+    int emotional,nervous,lazy,berserk,diligent,fearless;
+}
